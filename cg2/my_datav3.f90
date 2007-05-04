a7f5894d2ca8d497d1fb4e70a22efe3c82fc7f47
Lars Vilhuber <lars.vilhuber@cornell.edu>
2007-05-04 17:37:44 +0000

    Copying over the v3 source code for starters

---
+++ cg2/my_datav3.f90
@@ -0,0 +1,20 @@
+
+      MODULE MY_DATA
+      INTEGER n,ncells,npers,nfirm,ncov,ncoef,ncoef2
+
+      INTEGER, ALLOCATABLE :: pers(:),firm(:),dfp(:),dff(:)
+
+      REAL*8, ALLOCATABLE :: covin(:),y(:),d(:),f(:),df(:), xx(:,:), &
+        rq(:,:),xd(:,:),xf(:,:),theta(:),theta2(:),b(:),b2(:), &
+        r(:),w(:),p(:),q(:),u1(:),covmean(:)
+
+      REAL*4, ALLOCATABLE :: covin4(:)      
+
+      END MODULE MY_DATA
+
+
+
+
+
+
+
